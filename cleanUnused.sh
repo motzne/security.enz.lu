579904c2b5b6c6271311e659baf97f8f9e0fd378
Tom Enz <t@tomenz.ch>
2020-02-04 22:25:47 +0100

    Added another flavor to remove

--- cleanUnused.sh
+++ cleanUnused.sh
@@ -5,7 +5,7 @@
 #
 # remove page-ref
 find . -type f -iname "*.md" -exec sed -i.bak -E -e 's#^{% page-ref.*%}##g' {} \;
-
+find . -type f -iname "*.md" -exec sed -i.bak -E -e 's#^{% file src.*%}##g' {} \;
 
 
 find . -type f -name "*.bak" -delete
