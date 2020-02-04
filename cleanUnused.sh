eed94ea148dfd655b2c485ced8ce7a6dc41b7548
Tom Enz <t@tomenz.ch>
2020-02-04 08:09:26 +0100

    Added helper tool for unused local serves

---
+++ cleanUnused.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Simple Helper Tool for local serving gitbook
+#
+#
+# remove page-ref
+find . -type f -iname "*.md" -exec sed -i.bak -E -e 's#^{% page-ref.*%}##g' {} \;
+
+
+
+find . -type f -name "*.bak" -delete
